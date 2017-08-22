b10c10ddc6d051cfd6b490f9cf1d4365418cff17
RyoheiShoji <ryohei.shoji.sys@gmail.com>
2017-08-22 11:57:12 +0900

    Update AppSeizaUranai_EF.java

--- src/AppSeizaUranai_EF.java
+++ src/AppSeizaUranai_EF.java
@@ -98,6 +98,55 @@ public class AppSeizaUranai_EF {
 	}
 	public static void gemini(int rank){
 		//番屋 翔さん
+		if(rank == 1){
+			System.out.println("恋のビッグチャンス到来でモテモテ。気になる人には積極的なアピールを");
+			System.out.println("ラッキーポイント"+ "インテリア雑貨ショップ");
+		}
+		else if(rank == 2){
+			System.out.println("ユニークな発想で注目の的。迷わず提案してチャレンジ");
+			System.out.println("アイスレモンティー");
+		}
+		else if(rank == 3){
+			System.out.println("好きな人とバッタリ遭遇。古風な考え方で好感度↑");
+			System.out.println("フランスの曲");
+		}
+		else if(rank == 4){
+			System.out.println("ステキな文章がひらめく日。手書きの手紙がポイント！");
+			System.out.println("演歌");
+		}
+		else if(rank == 5){
+			System.out.println("新たな道が開ける予感♪好奇心を持つことが大切");
+			System.out.println("ミネラルウオーター");
+		}
+		else if(rank == 6){
+			System.out.println("うれしい知らせがありそう。電話は早めに取るのが正解");
+			System.out.println("ベランダ");
+		}
+		else if(rank == 7){
+			System.out.println("過去のトラブルで大混乱。課題を洗い出して即解決");
+			System.out.println("経済ニュース");
+		}
+		else if(rank == 8){
+			System.out.println("人間関係が悪化して孤立。素直に気持ちを表現して");
+			System.out.println("大判のハンカチ");
+		}
+		else if(rank == 9){
+			System.out.println("口だけの約束で評価↓。有言実行の姿勢が大事");
+			System.out.println("雑誌");
+		}
+		else if(rank == 10){
+			System.out.println("知識が足りずにアタフタ。勉強をやり直して再挑戦");
+			System.out.println("無地のノート");
+		}
+		else if(rank == 11){
+			System.out.println("何をやっても空回りして疲れ気味…おいしいものを食べてリフレッシュ");
+			System.out.println("動画サイト");
+		}
+		else if(rank == 12){
+			System.out.println("物事を決められずイライラ。迷ったときは時間をおいてみて！");
+			System.out.println("白い便箋");
+		}
+	}
 	}
 	public static void cancer(int rank){
 		//庄司
